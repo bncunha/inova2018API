f62a9c0356613177c29b840bfa9e858eebcc890f
Bruno Cunha <rocks.bn@gmail.com>
2018-07-22 23:49:20 -0300

    commit inicial

---
+++ api/models/Teste.js
@@ -0,0 +1,15 @@
+/**
+ * Teste.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    usuario: {type: "string", required: true}
+  },
+
+};
+
